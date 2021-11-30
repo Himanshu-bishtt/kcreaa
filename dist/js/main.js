67454fa6b440397b14cba741a815133d8089fe07
Himanshu Bisht <himanshubisht313@gmail.com>
2021-11-30 11:37:49 +0530

    changed css, sticky header, improve navigation links scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -56,3 +56,38 @@ topbarClose.addEventListener("click", function (e) {
   e.preventDefault();
   document.querySelector(".topbar").classList.add("remove-topbar");
 });
+
+// Navigation scroll
+
+const navigationLinks = document.getElementsByClassName("navigation__link");
+
+for (let i = 1; i < navigationLinks.length; ++i) {
+  navigationLinks[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    const navigationContent = navigationLinks[i].textContent.toLowerCase();
+    switch (navigationContent) {
+      case "work":
+        window.scroll(0, 780);
+        break;
+      case "gallery":
+        window.scroll(0, 1480);
+        break;
+      case "contact":
+        window.scroll(0, 3200);
+        break;
+    }
+  });
+}
+
+// Sticky navgiation
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    header.style.boxShadow = "0rem 1rem 1rem rgba(0, 0, 0, 0.1)";
+    header.style.opacity = "0.95";
+  } else {
+    header.style.boxShadow = "0rem 0rem 0rem rgba(0, 0, 0, 0.1)";
+    header.style.opacity = "1";
+  }
+});
